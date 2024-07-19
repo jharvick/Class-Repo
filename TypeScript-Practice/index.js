69b057fc23d272c188c4673b883e202e6112f993
jared.harvick <jared.harvick@cvent.com>
2024-07-19 11:19:57 -0500

    Completed Product and InventoryItem

--- TypeScript-Practice/index.js
+++ TypeScript-Practice/index.js
@@ -1,14 +1,28 @@
-var mountains = [];
-mountains.push({ name: 'Kilimanjaro', height: 19341 });
-mountains.push({ name: 'Everest', height: 29029 });
-mountains.push({ name: 'Denali', height: 20310 });
-function findNameOfTallestMountain(mountains) {
-    var tallestMountain = mountains[0];
-    mountains.forEach(function (mountain) {
-        if (mountain.height > tallestMountain.height) {
-            tallestMountain = mountain;
-        }
+// interface Mountain {
+//     name: string;
+//     height: number;
+// }
+var products = [];
+products.push({ name: 'Laptop', price: 1000 });
+products.push({ name: 'Phone', price: 500 });
+products.push({ name: 'Tablet', price: 200 });
+function calcAverageProductPrice(products) {
+    var totalPrice = 0;
+    products.forEach(function (product) {
+        totalPrice += product.price;
     });
-    return tallestMountain.name;
+    return totalPrice / products.length;
 }
-console.log(findNameOfTallestMountain(mountains)); // Everest
+console.log(calcAverageProductPrice(products));
+var inventory = [];
+inventory.push({ product: { name: 'motor', price: 10.00 }, quantity: 10 });
+inventory.push({ product: { name: 'sensor', price: 12.50 }, quantity: 4 });
+inventory.push({ product: { name: 'LED', price: 1.00 }, quantity: 20 });
+function calcInventoryValue(inventory) {
+    var totalValue = 0;
+    inventory.forEach(function (item) {
+        totalValue += item.product.price * item.quantity;
+    });
+    return totalValue;
+}
+console.log(calcInventoryValue(inventory));
--- TypeScript-Practice/index.ts
+++ TypeScript-Practice/index.ts
@@ -1,22 +1,64 @@
-interface Mountain {
+// interface Mountain {
+//     name: string;
+//     height: number;
+// }
+
+// const mountains: Mountain[] = [];
+
+// mountains.push({ name: 'Kilimanjaro', height: 19341 });
+// mountains.push({ name: 'Everest', height: 29029 });
+// mountains.push({ name: 'Denali', height: 20310 });
+
+// function findNameOfTallestMountain(mountains: Mountain[]): string {
+//     let tallestMountain = mountains[0];
+//     mountains.forEach(mountain => {
+//         if (mountain.height > tallestMountain.height) {
+//             tallestMountain = mountain;
+//         }
+//     });
+//     return tallestMountain.name;
+// }
+
+// console.log(findNameOfTallestMountain(mountains));
+
+interface Product {
     name: string;
-    height: number;
+    price: number;
 }
 
-const mountains: Mountain[] = [];
+const products: Product[] = [];
 
-mountains.push({ name: 'Kilimanjaro', height: 19341 });
-mountains.push({ name: 'Everest', height: 29029 });
-mountains.push({ name: 'Denali', height: 20310 });
+products.push({ name: 'Laptop', price: 1000 });
+products.push({ name: 'Phone', price: 500 });
+products.push({ name: 'Tablet', price: 200 });
 
-function findNameOfTallestMountain(mountains: Mountain[]): string {
-    let tallestMountain = mountains[0];
-    mountains.forEach(mountain => {
-        if (mountain.height > tallestMountain.height) {
-            tallestMountain = mountain;
-        }
+function calcAverageProductPrice(products: Product[]): number {
+    let totalPrice = 0;
+    products.forEach(product => {
+        totalPrice += product.price;
     });
-    return tallestMountain.name;
+    return totalPrice / products.length;
 }
 
-console.log(findNameOfTallestMountain(mountains)); // Everest
+console.log(calcAverageProductPrice(products));
+
+interface InventoryItem {
+    product: Product;
+    quantity: number;
+}
+
+const inventory: InventoryItem[] = [];
+
+inventory.push({ product: { name: 'motor', price: 10.00 }, quantity: 10 });
+inventory.push({ product: { name: 'sensor', price: 12.50 }, quantity: 4 });
+inventory.push({ product: { name: 'LED', price: 1.00 }, quantity: 20 });
+
+function calcInventoryValue(inventory: InventoryItem[]): number {
+    let totalValue = 0;
+    inventory.forEach(item => {
+        totalValue += item.product.price * item.quantity;
+    });
+    return totalValue;
+}
+
+console.log(calcInventoryValue(inventory));
